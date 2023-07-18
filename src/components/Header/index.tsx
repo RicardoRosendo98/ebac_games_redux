88799f1932428bacaaac2fe4c3606f38779e1ec3
Ricardo Rosendo <88691117+RicardoRosendo98@users.noreply.github.com>
2023-07-18 14:45:12 -0300

    trabalhando com Test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,10 +5,10 @@ import * as S from './styles'
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
 
-import { RootReducer } from '../../store'
+import { RootState } from '../../store'
 
 const Header = () => {
-  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+  const itens = useSelector((state: RootState) => state.carrinho.itens)
 
   const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
@@ -21,7 +21,8 @@ const Header = () => {
       <div>
         <img src={cesta} />
         <span>
-          {itens.length} itens, valor total: {paraReal(valorTotal)}
+          <span data-testid="qtd-carrinho">{itens.length} itens</span>, valor
+          total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
